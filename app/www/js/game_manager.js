bc32d960f795479e75a41155bb2456ccc42431e7
ctllo-bit <test@example.com>
2026-06-05 10:26:17 +0800

    over

--- app/www/js/game_manager.js
+++ app/www/js/game_manager.js
@@ -1,17 +1,16 @@
 function GameManager(size, InputManager, Actuator, StorageManager) {
   this.size           = size; // Size of the grid
-  // this.inputManager   = new InputManager;
-  // this.storageManager = new StorageManager;
-  // this.actuator       = new Actuator;
+  this.inputManager   = new InputManager;
+  this.storageManager = new StorageManager;
+  this.actuator       = new Actuator;
 
-  // this.startTiles     = 2;
+  this.startTiles     = 2;
 
-  // this.inputManager.on("move", this.move.bind(this));
-  // this.inputManager.on("restart", this.restart.bind(this));
-  // this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
+  this.inputManager.on("move", this.move.bind(this));
+  this.inputManager.on("restart", this.restart.bind(this));
+  this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
 
-  // this.setup();
-  console.log("sfdsddddddddddddssssssssss111111111ddddddddddddddddddd");
+  this.setup();
 }
 
 // Restart the game
--- app/www/js/html_actuator.js
+++ app/www/js/html_actuator.js
@@ -1,17 +1,18 @@
-// html_actuator.js
 function HTMLActuator() {
-  this.tileContainer = document.querySelector(".tile-container");
-  this.scoreContainer = document.querySelector(".score-value");
+  this.tileContainer    = document.querySelector(".tile-container");
+  this.scoreContainer   = document.querySelector(".score-container");
+  this.bestContainer    = document.querySelector(".best-container");
+  this.messageContainer = document.querySelector(".game-message");
+
+  this.score = 0;
 }
 
 HTMLActuator.prototype.actuate = function (grid, metadata) {
   var self = this;
 
-  // 1. 每次渲染前，清空旧的方块 DOM
   window.requestAnimationFrame(function () {
     self.clearContainer(self.tileContainer);
 
-    // 2. 遍历网格里的每一个方块实体
     grid.cells.forEach(function (column) {
       column.forEach(function (cell) {
         if (cell) {
@@ -20,35 +21,119 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
       });
     });
 
-    // 3. 更新分数等 UI
-    self.scoreContainer.innerText = metadata.score;
+    self.updateScore(metadata.score);
+    self.updateBestScore(metadata.bestScore);
+
+    if (metadata.terminated) {
+      if (metadata.over) {
+        self.message(false); // You lose
+      } else if (metadata.won) {
+        self.message(true); // You win!
+      }
+    }
+
   });
 };
 
+// Continues the game (both restart and keep playing)
+HTMLActuator.prototype.continueGame = function () {
+  this.clearMessage();
+};
+
+HTMLActuator.prototype.clearContainer = function (container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+
 HTMLActuator.prototype.addTile = function (tile) {
-  var wrapper = document.createElement("div");
-  var inner   = document.createElement("div");
+  var self = this;
 
-  // 将方块的当前坐标转换为 CSS 类名（触发移动动画的关键）
-  var positionClass = "tile-position-" + tile.x + "-" + tile.y;
+  var wrapper   = document.createElement("div");
+  var inner     = document.createElement("div");
+  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+  var positionClass = this.positionClass(position);
 
-  // 基本样式
+  // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  // 如果它是一个新生成的方块，加上出现动画类名
-  if (tile.previousPosition === null && !tile.mergedFrom) {
-    classes.push("tile-new");
-  } 
-  // 如果是合并出来的，加上弹跳动画类名
-  else if (tile.mergedFrom) {
+  if (tile.value > 2048) classes.push("tile-super");
+
+  this.applyClasses(wrapper, classes);
+
+  inner.classList.add("tile-inner");
+  inner.textContent = tile.value;
+
+  if (tile.previousPosition) {
+    // Make sure that the tile gets rendered in the previous position first
+    window.requestAnimationFrame(function () {
+      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+      self.applyClasses(wrapper, classes); // Update the position
+    });
+  } else if (tile.mergedFrom) {
     classes.push("tile-merged");
+    this.applyClasses(wrapper, classes);
+
+    // Render the tiles that merged
+    tile.mergedFrom.forEach(function (merged) {
+      self.addTile(merged);
+    });
+  } else {
+    classes.push("tile-new");
+    this.applyClasses(wrapper, classes);
   }
 
-  // 拼接类名并渲染到屏幕上
-  wrapper.className = classes.join(" ");
-  inner.className = "tile-inner";
-  inner.innerText = tile.value;
-
+  // Add the inner part of the tile to the wrapper
   wrapper.appendChild(inner);
+
+  // Put the tile on the board
   this.tileContainer.appendChild(wrapper);
+};
+
+HTMLActuator.prototype.applyClasses = function (element, classes) {
+  element.setAttribute("class", classes.join(" "));
+};
+
+HTMLActuator.prototype.normalizePosition = function (position) {
+  return { x: position.x + 1, y: position.y + 1 };
+};
+
+HTMLActuator.prototype.positionClass = function (position) {
+  position = this.normalizePosition(position);
+  return "tile-position-" + position.x + "-" + position.y;
+};
+
+HTMLActuator.prototype.updateScore = function (score) {
+  this.clearContainer(this.scoreContainer);
+
+  var difference = score - this.score;
+  this.score = score;
+
+  this.scoreContainer.textContent = this.score;
+
+  if (difference > 0) {
+    var addition = document.createElement("div");
+    addition.classList.add("score-addition");
+    addition.textContent = "+" + difference;
+
+    this.scoreContainer.appendChild(addition);
+  }
+};
+
+HTMLActuator.prototype.updateBestScore = function (bestScore) {
+  this.bestContainer.textContent = bestScore;
+};
+
+HTMLActuator.prototype.message = function (won) {
+  var type    = won ? "game-won" : "game-over";
+  var message = won ? "You win!" : "Game over!";
+
+  this.messageContainer.classList.add(type);
+  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+};
+
+HTMLActuator.prototype.clearMessage = function () {
+  // IE only takes one value to remove at a time.
+  this.messageContainer.classList.remove("game-won");
+  this.messageContainer.classList.remove("game-over");
 };
